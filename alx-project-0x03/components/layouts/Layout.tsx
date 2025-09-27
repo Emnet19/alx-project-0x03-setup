8a9202851d866ba62c6c0563c61a9101c39a9515
Emnet Befkadu <emnbef456@gmail.com>
2025-09-28 00:16:59 +0300

    m

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,47 +1,3 @@
-// import { ReactNode } from "react";
-// import Footer from "./Footer";
-// import Header from "./Header";
-
-// interface LayoutProps {
-//   children: ReactNode;
-// }
-
-// const Layout: React.FC<LayoutProps> = ({ children }) => {
-//   return (
-//     <>
-//       <Header />
-//       <main>{children}</main>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default Layout;
-
-
-
-
-// import { LayoutProps } from "@/interface";
-
-// export default function Layout({ children }: LayoutProps) {
-//   return (
-//     <div>
-//       {/* Header */}
-//       <header className="bg-gray-800 text-white p-4">Splash App Header</header>
-
-//       {/* Page Content */}
-//       <main>{children}</main>
-
-//       {/* Footer */}
-//       <footer className="bg-gray-800 text-white p-4 mt-8 text-center">
-//         Splash App Footer
-//       </footer>
-//     </div>
-//   );
-// }
-
-
-
 import { LayoutProps } from "@/interface";
 
 export default function Layout({ children }: LayoutProps) {
@@ -53,3 +9,4 @@ export default function Layout({ children }: LayoutProps) {
     </div>
   );
 }
+
--- alx-project-0x03/interface/index.tsx
+++ alx-project-0x03/interface/index.tsx
@@ -13,6 +13,27 @@
 // }
 
 
+// // interface/index.ts
+// import { ReactNode } from "react";
+
+// // Interface for page routing
+// export interface PageRouteProps {
+//   pageRoute: string;
+// }
+
+// // Interface for the Button component
+// export interface ButtonProps {
+//   action: () => void;
+//   buttonLabel: string;
+//   buttonBackgroundColor: "red" | "blue" | "green" | "orange";
+// }
+
+// // Interface for Layout component
+// export interface LayoutProps {
+//   children: ReactNode;
+// }
+
+
 // interface/index.ts
 import { ReactNode } from "react";
 
